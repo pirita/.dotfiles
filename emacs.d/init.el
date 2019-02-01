d3f632be39719f4444c85ff0951cde059fba4040
Ignacio Navarro <3074688+pirita@users.noreply.github.com>
2019-02-01 12:14:05 +0100

    Add projectile and neotree

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -71,11 +71,6 @@
 (use-package rainbow-delimiters
   :config (progn (add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode)))
 
-;; Neo-tree
-(use-package neotree
-  :defer t
-  :bind ("<f8>" . neotree-toggle))
-
 (use-package nlinum
   :config (add-hook 'prog-mode-hook '(lambda () (nlinum-mode t))))
 
@@ -85,13 +80,61 @@
 (use-package scala-mode
   :pin melpa)
 
-(use-package darkokai-theme
-  :config (load-theme 'darkokai t))
+(use-package all-the-icons)
 
 (add-to-list 'exec-path "/usr/local/bin")
 
 (use-package auto-package-update
   :config (setq auto-package-update-delete-old-versions t))
+
+(use-package projectile
+  :demand
+  :bind-keymap ("C-c p" . projectile-command-map)
+  :config
+  (setq projectile-enable-caching nil
+        projectile-indexing-method 'alien
+		projectile-project-root-files '(".git" ".project" "setup.py" "build.sbt" "pom.xml")
+        projectile-globally-ignored-file-suffixes '(".elc" ".pyc" ".o" ".class")
+        projectile-globally-ignored-files '(".DS_Store" "Icon")))
+
+(use-package neotree
+  :defer t
+  :functions (neotree-resize-window neotree-project-dir)
+  :commands neotree-project-dir
+  :hook ((neo-enter . neotree-resize-window))
+  :bind ("<f8>" . 'neotree-project-dir)
+  :config
+
+  (defun neotree-resize-window (&rest _args)
+    "Resize neotree window."
+    (neo-global--when-window
+      (let ((fit-window-to-buffer-horizontally t))
+        (neo-buffer--unlock-width)
+        (fit-window-to-buffer)
+        (neo-buffer--lock-width))))
+
+  (defun neotree-project-dir ()
+    "Open NeoTree using the git root."
+    (interactive)
+    (let ((project-dir (projectile-project-root))
+          (file-name (buffer-file-name)))
+      (neotree-toggle)
+      (if project-dir
+          (if (neo-global--window-exists-p)
+              (progn
+                (neotree-dir project-dir)
+                (neotree-find file-name))))))
+
+  (setq neo-theme 'nerd
+        neo-window-width 40
+        neo-create-file-auto-open t
+        neo-show-updir-line nil
+        neo-mode-line-type 'neotree
+        neo-smart-open t
+        neo-autorefresh t
+        neo-auto-indent-point t
+        neo-show-hidden-files t
+        neo-window-fixed-size nil))
 
 ;;;;;;;
 ;; Configuration
